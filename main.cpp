c40b4bc14cd00648968a03a7c61d75720d6bffe0
Róbert Fuszenecker <robert.fuszenecker@outlook.com>
2018-05-26 19:27:02 +0200

    Better string representations, examples

--- main.cpp
+++ main.cpp
@@ -6,45 +6,50 @@
 
 using namespace std;
 
+namespace string_converter {
+    template <typename CharType>
+    basic_string<CharType> from_string(string s) {
+        return std::wstring_convert<std::codecvt_utf8_utf16<CharType>, CharType>{}.from_bytes(s);
+    }
+
+    template <typename CharType>
+    static std::string to_string(basic_string<CharType> s) {
+        return std::wstring_convert<std::codecvt_utf8_utf16<CharType>, CharType>{}.to_bytes(s);
+    }
+}
+
 template<typename CharType = char>
-class MyClassT {
+class MyClassGeneric {
     public:
         basic_string<CharType> name;
-
-        static basic_string<CharType> from_string(string s) {
-            return std::wstring_convert<std::codecvt_utf8_utf16<CharType>, CharType>{}.from_bytes(s);
-        }
-
-        static std::string to_string(basic_string<CharType> s) {
-            return std::wstring_convert<std::codecvt_utf8_utf16<CharType>, CharType>{}.to_bytes(s);
-        }
 };
 
-typedef MyClassT<> MyClassS;
+typedef MyClassGeneric<> MyClassDefault;
 
 int main()
 {
-    auto g = MyClassS { "My name" };
-    auto h = MyClassT<wchar_t> {};
+    auto class_utf8 = MyClassDefault { "My name" };
+    auto class_ucs4 = MyClassGeneric<char32_t> { U"Salve, Munde, őőő űűű" };
 
-    auto s = string { "Salve, Munde, éé áá őő űű" };
+    auto utf8_string = string { "Salve, Munde, éé áá őő űű" };
 
-    cout << "From UTF-8 const char*: ";
+    cout << "UTF-8 string: " << utf8_string << endl;
+    cout << "UTF-8 as const char*: ";
 
-    for (auto ch: h.from_string("őőűű")) {
+    for (auto ch: utf8_string) {
         cout << ch << " " << flush;
     }
 
-    cout << endl << "From std::string: ";
+    cout << endl << "UCS2 string from std::string: ";
 
-    auto j = h.from_string(s);
+    auto ucs2_string = string_converter::from_string<char16_t>(utf8_string);
 
-    for (auto ch: j) {
+    for (auto ch: ucs2_string) {
         cout << ch << " " << flush;
     }
 
     cout << endl;
 
-    cout << "UTF-8 length: " << s.length() << ", UTF-16 length: " << j.length() << endl;
-    cout << "UTF-8: " << h.to_string(j) << endl;
+    cout << "UTF-8 length: " << utf8_string.length() << ", UCS2 length: " << ucs2_string.length() << endl;
+    cout << "Verificarion: UTF-8: " << string_converter::to_string<char16_t>(ucs2_string) << endl;
 }
